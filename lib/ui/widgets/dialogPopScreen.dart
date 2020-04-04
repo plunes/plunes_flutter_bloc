2520e196e6eb4dfa6a254ca9425e8833e0603dbf
plunes <plunestechnologies@plunes.com>
2020-04-04 14:49:33 +0530

    add popup

--- lib/ui/widgets/dialogPopScreen.dart
+++ lib/ui/widgets/dialogPopScreen.dart
@@ -98,3 +98,91 @@ Widget buildAboutDialog({
 });
 }
 }
+
+
+Widget buildProfileDialog({
+  @required final String dialogTitle,
+  @required final String dialogMsg,
+ }) {
+    return StatefulBuilder(builder: (context, newState) {
+    return Dialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0)),
+        elevation: 0.0,
+        //backgroundColor: Colors.transparent,
+        child: dialogProfileContent(context),
+        );
+ });
+
+}
+
+ Widget dialogProfileContent(BuildContext context) {
+        return Container(
+       // padding: EdgeInsets.only(left: 25, right: 25),
+
+        margin: EdgeInsets.all(20),
+        child: Stack(
+            alignment: Alignment.topCenter,
+            children: <Widget>[
+             Text('Profile',textAlign: TextAlign.center, 
+             style: TextStyle(fontSize:20, fontWeight: FontWeight.bold),
+             ),
+            Container(
+                padding: EdgeInsets.only(top: 18.0),
+                margin: EdgeInsets.only(top: 13.0,right: 8.0),
+                child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                 SizedBox(height: 20.0,),
+                 Row(
+                   children: <Widget>[
+                     Container(
+                      width: 50.0,
+                      height: 50.0,
+                      margin: EdgeInsets.all(10),
+                      decoration: new BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: new DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image: new NetworkImage(
+                                      "https://i.imgur.com/BoN9kdC.png")
+                              )
+                      )),
+
+                      Column(
+                        //mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text('Anchal Sherawat', style:TextStyle(fontWeight: FontWeight.bold)),
+                          Text('dermologist Consultain',style:TextStyle(color:Colors.black45), ),
+                          Text('Address: haryana ', style:TextStyle(color:Colors.black45), 
+                          )
+                        ],
+                      )
+                   ],
+                 ),
+                  Divider(color:Colors.black54),
+
+                  Text('Available Slots')
+                  
+                ],
+                ),
+            ),
+            Positioned(
+                right: 0.0,
+                child: Align(
+                alignment: Alignment.topRight, 
+                child: IconButton(
+                icon:Icon(Icons.close),
+                 onPressed: () => {
+                    Navigator.of(context).pop(),
+                  }, 
+               ),  
+              ), 
+            ),
+            ],
+        ),
+        );
+    }
+
+
