a887b7a028122e4f61da55c2c7c5de13695aae42
plunes <plunestechnologies@plunes.com>
2020-04-04 12:34:17 +0530

    add popup

--- lib/ui/widgets/dialogPopScreen.dart
+++ lib/ui/widgets/dialogPopScreen.dart
@@ -17,27 +17,84 @@ Widget buildAboutDialog({
   @required final String dialogMsg,
  }) {
     return StatefulBuilder(builder: (context, newState) {
-    return new AlertDialog(
-      
-      title: Text(dialogTitle),
-      content: new Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-          child:Text(dialogMsg),
-          )
+    return AlertDialog(
+        contentPadding: EdgeInsets.only(left: 25, right: 25),
+         shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20.0))
+            ),
+
+        title: Text(dialogTitle,style: TextStyle(),
+        textAlign: TextAlign.center, 
+        ),
+        content: Container(
+          margin: EdgeInsets.symmetric(vertical:20),
+          height:300,
+          width: 300,
+         child: SingleChildScrollView(
+                child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+               children: <Widget>[
+                 
+                //  SizedBox(
+                //       height: 0,
+                //     ),
+                 
+                  Text( 'Defination:', style: TextStyle(fontWeight: FontWeight.bold),),
+                  Text( 'Dermologist Consultation' , style: TextStyle(
+                     color: Colors.black38,
+                  ),),
+
+                    Divider(
+                         color: Colors.black45,
+                       ),
+                   Text( 'Duration', style: TextStyle(fontWeight: FontWeight.bold),),
+                    Text( 'Space for text' , style: TextStyle(
+                     color: Colors.black45,
+                  ),),
+
+                       Divider(
+                         color: Colors.black45,
+                       ),
+
+                    Text( 'Sittings:', style: TextStyle(fontWeight: FontWeight.bold),),
+                     Text( '2' , style: TextStyle(
+                     color: Colors.black38,
+                  ),),
+                  
+                  Divider(
+                         color: Colors.black45,
+                       ),  
+
+                    Text( 'Do\'s and Don\'t:', style: TextStyle(fontWeight: FontWeight.bold),),
+                    Text( '1.Dermologist Consultation, Dermologist Consultation, Dermologist Consultation' , style: TextStyle(
+                     color: Colors.black38,
+                   ),),
+                   Text( '1.Dermologist Consultation, Dermologist Consultation, Dermologist Consultation' , style: TextStyle(
+                     color: Colors.black38,
+                  ),),
+                   Text( '1.Dermologist Consultation, Dermologist Consultation, Dermologist Consultation' , style: TextStyle(
+                     color: Colors.black38,
+                  ),),
+               
+              ],
+            ),
+          ),
+        ),
+        
+        actions: <Widget>[
+          Text('Expand', style: TextStyle(fontSize: 18),
+          textAlign: TextAlign.center,),
+          new IconButton(
+             alignment: Alignment.center,
+             icon:Icon(Icons.expand_more),
+             onPressed: () => {
+                Navigator.of(context).pop(),
+             }, 
+           ),
         ],
-      ),
-      actions: <Widget>[
-       // AddButton(null,'Continue',100),
-        new RaisedButton(
-          onPressed: () => {
-            Navigator.of(context).pop(),
-          }),
-      ],
-    );
+
+
+      );
+});
 }
-);
- }
 }
