a227e86dae8cfa066d077c9690233360e8542f78
Gautam Singh Maurya <gsmaurya2014@gmail.com>
2023-06-05 20:56:57 +0530

    after submit otp screen goes to blank for few users - fixed.

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/gautamsinghmaurya/Freelance/flutter_3.3.10"
-export "FLUTTER_APPLICATION_PATH=/Users/gautamsinghmaurya/Freelance/plunes/Code/manage_branch/latest_code/plunes_flutter_bloc"
+export "FLUTTER_APPLICATION_PATH=/Users/gautamsinghmaurya/Freelance/plunes/code/plunes_flutter_bloc"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
