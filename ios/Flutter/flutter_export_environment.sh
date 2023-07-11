1a68121063820e42fe4635fdc2360d2fa62e0fed
Gautam Singh Maurya <gsmaurya2014@gmail.com>
2023-07-11 06:45:07 +0530

    iOS+Android build is pushed

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/gautamsinghmaurya/Freelance/flutter_3.3.10"
-export "FLUTTER_APPLICATION_PATH=/Users/gautamsinghmaurya/Freelance/plunes/code/plunes_flutter_bloc"
+export "FLUTTER_ROOT=/Users/gautamsinghmaurya/Freelance/flutter_3.3.3"
+export "FLUTTER_APPLICATION_PATH=/Users/gautamsinghmaurya/Freelance/plunes/code/plunes_new/plunes_flutter_bloc"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.2.43"
-export "FLUTTER_BUILD_NUMBER=83"
+export "FLUTTER_BUILD_NAME=1.2.44"
+export "FLUTTER_BUILD_NUMBER=84"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
