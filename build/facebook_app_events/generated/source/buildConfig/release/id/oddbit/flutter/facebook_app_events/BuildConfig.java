8222976a5ac42722b470a7068520898e40c4f98f
Gautam Singh Maurya <gsmaurya2014@gmail.com>
2023-07-22 12:00:23 +0530

    1.2.44 android build released.

---
+++ build/facebook_app_events/generated/source/buildConfig/release/id/oddbit/flutter/facebook_app_events/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package id.oddbit.flutter.facebook_app_events;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "id.oddbit.flutter.facebook_app_events";
+  public static final String BUILD_TYPE = "release";
+}
